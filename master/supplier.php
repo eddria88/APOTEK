44087ddf2167a66a944c688be953ac2c39f67d04
ｓｓａａ ₍^. .^₎⟆ <lyyssaa991@gmail.com>
2026-04-22 11:52:29 +0700

    supplier baru

--- master/supplier.php
+++ master/supplier.php
@@ -171,13 +171,18 @@ $dataResult = mysqli_query($conn, "SELECT * FROM supplier $where ORDER BY id_sup
 
         <!-- SIDEBAR -->
         <aside class="sidebar">
+              <?php if ($user['role'] != 'admin'): ?>
             <div class="sb-sec">Core</div>
             <a class="sb-link" href="../dashboard.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+            <?php endif; ?>
             <div class="sb-sec">Master Data</div>
-            <a class="sb-link" href="kategori.php"><i class="fas fa-tags"></i> Kategori</a>
-            <a class="sb-link active" href="supplier.php"><i class="fas fa-truck"></i> Supplier</a>
+            <a class="sb-link active" href="kategori.php"><i class="fas fa-tags"></i> Kategori</a>
+            <?php if ($user['role'] != 'kasir'): ?>
+            <a class="sb-link" href="supplier.php"><i class="fas fa-truck"></i> Supplier</a>
+            <?php endif; ?>
             <a class="sb-link" href="obat.php"><i class="fas fa-pills"></i> Obat</a>
             <a class="sb-link" href="member.php"><i class="fas fa-user-friends"></i> Member</a>
+            <?php if ($user['role'] == 'owner'): ?>
             <div class="sb-sec">Transaksi</div>
             <a class="sb-link" href="../transaksi/pembelian.php"><i class="fas fa-shopping-bag"></i> Pembelian</a>
             <a class="sb-link" href="../transaksi/penjualan.php"><i class="fas fa-cash-register"></i> Penjualan</a>
@@ -185,6 +190,10 @@ $dataResult = mysqli_query($conn, "SELECT * FROM supplier $where ORDER BY id_sup
             <a class="sb-link" href="../laporan/laporan_penjualan.php"><i class="fas fa-chart-line"></i> Penjualan</a>
             <a class="sb-link" href="../laporan/laporan_pembelian.php"><i class="fas fa-chart-bar"></i> Pembelian</a>
             <a class="sb-link" href="../laporan/laporan_stok.php"><i class="fas fa-boxes"></i> Stok</a>
+            <?php elseif ($user['role'] == 'kasir'): ?>
+            <div class="sb-sec">Transaksi</div>
+            <a class="sb-link" href="../transaksi/penjualan.php"><i class="fas fa-cash-register"></i> Penjualan</a>
+            <?php endif; ?>
             <div class="sb-footer">
                 <div class="small">Masuk sebagai</div>
                 <strong><?= htmlspecialchars($user['nama_user']) ?></strong>
